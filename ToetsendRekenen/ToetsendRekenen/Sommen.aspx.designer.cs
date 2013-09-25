b2661c4c61c357cf5b33c6db738f1bdd11eebcf5
wargamer88 <m.van.denzel@home.nl>
2013-09-25 12:10:56 +0200

    Statistiek toegevoegd

--- ToetsendRekenen/ToetsendRekenen/Sommen.aspx.designer.cs
+++ ToetsendRekenen/ToetsendRekenen/Sommen.aspx.designer.cs
@@ -20,5 +20,14 @@ namespace ToetsendRekenen {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// btnControleer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnControleer;
     }
 }
