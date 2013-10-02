8dc7fa7a252f11d9a654808101c43cfcb2d184ac
KingdomHearts <michael_bossink@hotmail.com>
2013-10-02 10:00:07 +0200

    DataToDatabase class is aangemaakt.

---
+++ ToetsendRekenen/ToetsendRekenen/DataToDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToetsendRekenen
+{
+    public class DataToDatabase
+    {
+        //#region Methodes
+        //public Image byteArrayToImage(byte[] byteArrayIn)
+        //{
+        //    MemoryStream ms = new MemoryStream(byteArrayIn);
+        //    Image returnImage = Image.FromStream(ms);
+        //    return returnImage;
+        //}
+
+        //public byte[] imageToByteArray(System.Drawing.Image imageIn)
+        //{
+        //    MemoryStream ms = new MemoryStream();
+        //    imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+        //    return ms.ToArray();
+        //}
+        //#endregion
+    }
+}
