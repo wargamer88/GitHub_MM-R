cdb404662ff9c7e76fb2e8e06840a7ee14cb6871
RGravity <rogieroh@hotmail.com>
2013-10-03 13:41:20 +0200

    Getallenlijn.cs aangemaakt en begonnen met methode

--- ToetsendRekenen/ToetsendRekenen/Getallenlijn.cs
+++ ToetsendRekenen/ToetsendRekenen/Getallenlijn.cs
@@ -7,5 +7,27 @@ namespace ToetsendRekenen
 {
     public class Getallenlijn
     {
+        public int[] GetallenlijnGenereren(string subcategorie, string moeilijkheidsgraad)
+        {
+            int[] Getallenlijn;
+            int StartGetal;
+            int EindGetal;
+            int VraagGetal;
+            if(moeilijkheidsgraad == "0-100")
+            {
+                Random R = new Random();
+                StartGetal = R.Next(0, 100);
+                EindGetal = R.Next(StartGetal, 100);
+                VraagGetal = R.Next(StartGetal, EindGetal);
+
+            }
+
+            Getallenlijn[1] = StartGetal;
+            Getallenlijn[2] = EindGetal;
+
+
+
+            return testc;
+        }
     }
 }
