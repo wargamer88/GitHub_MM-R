fe6f54d016220cc548db85f6fd95e3bdc65644a0
KingdomHearts <michael_bossink@hotmail.com>
2013-10-03 12:10:50 +0200

    Supermarkt tellen werkt

--- ToetsendRekenen/ToetsendRekenen/Supermarkt.aspx.cs
+++ ToetsendRekenen/ToetsendRekenen/Supermarkt.aspx.cs
@@ -10,22 +10,24 @@ namespace ToetsendRekenen
 {
     public partial class WebForm6 : System.Web.UI.Page
     {
-        
-        int[] price;
+
+        decimal[] price;
+        decimal sum;
         protected void Page_Load(object sender, EventArgs e)
         {
             string IpassAstringfrompage1 = Convert.ToString(Session["test"]);
             reaction.Text = IpassAstringfrompage1;
             string[,] producten = new string[,] { { "Appels", "2,69" }, { "Bananen", "2,19" }, { "Melk", "0,83" }, { "Pruimen", "1,41" }, { "Manderijnen", "1,55" }, { "Appelsap", "1,06" }, { "Chocola", "1,87" }, { "Eieren", "1,29" }, { "Peren", "1,69" }, { "Koekjes", "0,81" }, { "Rode Bieten", "1,13" }, { "Blik groente", "1,39" }, { "Thee", "0,93" }, { "Ananas", "1,99" }, { "Frisdrank", "0,39" } }; 
-            int i =0;
-            
-            int p = 0;
-            foreach (var t in producten){
-                int j =1;
-                var test = producten[i,j];
-                i++;
-                price[p] = Convert.ToInt32(test);
-                p++;
+            price = new decimal[producten.Length / 2];
+            int teller = 0;
+            for (int arr = 0; arr < producten.Length/2; arr++)
+            {
+                int j=1;
+                var test = producten[arr,j];
+                decimal test1  = Convert.ToDecimal(test);
+                price[teller] = test1;
+                teller++;
+                sum += price[arr];
             }
         }
 
@@ -36,7 +38,15 @@ namespace ToetsendRekenen
 
         protected void verzend_Click(object sender, EventArgs e)
         {
-            double antwoordvar = Convert.ToDouble(antwoord.Text);
+            decimal antwoordvar = Convert.ToDecimal(antwoord.Text);
+            if (antwoordvar == sum)
+            {
+                reaction.Text = "Het werkt!";
+            }
+            else
+            {
+                reaction.Text = "Klopt niet!";
+            }
         }
     }
 }
