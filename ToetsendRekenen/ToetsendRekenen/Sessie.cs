84a4555096751604a14ebd7de7707a7e41ba68b9
RGravity <rogieroh@hotmail.com>
2013-10-08 14:01:01 +0200

    Sessie.cs en Resultaat.cs

---
+++ ToetsendRekenen/ToetsendRekenen/Sessie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToetsendRekenen
+{
+    public class Sessie
+    {
+        public string SessieID { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
