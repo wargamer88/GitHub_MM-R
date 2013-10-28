4808702d2c333dd4b87e5422fd9ad7afbc2446e0
RGravity <rogieroh@hotmail.com>
2013-10-28 11:29:59 +0100

    Bug Fix 1

--- ToetsendRekenen/ToetsendRekenen/Sommen.aspx.designer.cs
+++ ToetsendRekenen/ToetsendRekenen/Sommen.aspx.designer.cs
@@ -103,6 +103,15 @@ namespace ToetsendRekenen {
         protected global::System.Web.UI.WebControls.Label lbResultaat;
         
         /// <summary>
+        /// btnControlleer control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnControlleer;
+        
+        /// <summary>
         /// btnVolgendeVraag control.
         /// </summary>
         /// <remarks>
